e4b021d744f0b6e51272ad9d4a866df87e5561c1
AratiMali2244 <maliarati2244@gmail.com>
2026-04-15 10:09:58 +0530

    Add files via upload

---
+++ code refactoring for improve readability and efficiency.py
@@ -0,0 +1,11 @@
+#Program before refactoring
+a=10
+b=20
+c=a+b
+print(c)
+#Porgram after refactoring
+#Program to add numbers
+num1=10
+num2=20
+sum_result=num1+num2
+print("Addition is:",sum_result)
